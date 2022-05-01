92d55771ba65e28f9fc270c09d9581b3e5b5e74a
nagatoxxx <72517497+nagatoxxx@users.noreply.github.com>
2022-05-01 11:20:46 +0300

    Add files via upload

---
+++ config/dunst/scripts/volume.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+volume=$(amixer -D pulse sget Master | tail -1 | awk -F'[][]' '{print $2}' | sed 's/%//g')
+
+dunstify -a "volume" -r 3456 -h int:value:"$volume" "volume: ${volume}%" -i /home/vlad/Pictures/Icons/volume-max.svg
